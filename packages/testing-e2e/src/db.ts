2d1fa9d6833652e627a8a63e66706a57ad363f9a
Adrian Topala <adrian.topala@gmail.com>
2025-06-25 11:57:16 +0200

    extending config

--- packages/testing-e2e/src/db.ts
+++ packages/testing-e2e/src/db.ts
@@ -1,9 +1,10 @@
 import postgres from "postgres";
 import { newOneSqlSchema } from "./codegen/one_sql.schema.js";
-import { POSTGRES_DATABASE, POSTGRES_PASSWORD, POSTGRES_USER } from "./config.js";
+import { POSTGRES_DATABASE, POSTGRES_PASSWORD, POSTGRES_USER, POSTGRES_HOST, POSTGRES_PORT } from "./config.js";
 
 export const sql = postgres({
-   host: "localhost",
+   host: POSTGRES_HOST,
+   port: POSTGRES_PORT,
    user: POSTGRES_USER,
    password: POSTGRES_PASSWORD,
    database: POSTGRES_DATABASE,
